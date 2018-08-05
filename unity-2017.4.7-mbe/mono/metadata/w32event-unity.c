199aa03b545eb4e4809e64a0f64c6bbc2e2772e2
de4dot <de4dot@gmail.com>
2018-08-05 12:09:41 +0200

    Add Unity files (unity-2017.4.7-mbe), commit hash 16e0200d3f1a3ae1d6ec548d14e527af267daafd

---
+++ unity-2017.4.7-mbe/mono/metadata/w32event-unity.c
@@ -0,0 +1,83 @@
+#include "w32event.h"
+#include "Handle-c-api.h"
+#include "Event-c-api.h"
+#include "Error-c-api.h"
+
+void
+mono_w32event_init (void)
+{
+}
+
+gpointer
+mono_w32event_create (gboolean manual, gboolean initial)
+{
+	UnityPalEvent* event = UnityPalEventNew(manual, initial);
+	return UnityPalEventHandleNew(event);
+}
+
+gboolean
+mono_w32event_close (gpointer handle)
+{
+	UnityPalHandleDestroy(handle);
+	return TRUE;
+}
+
+void
+mono_w32event_set (gpointer handle)
+{
+	UnityPalEventSet(UnityPalEventHandleGet(handle));
+}
+
+void
+mono_w32event_reset (gpointer handle)
+{
+	UnityPalEventReset(UnityPalEventHandleGet(handle));
+}
+
+gpointer
+ves_icall_System_Threading_Events_CreateEvent_internal (MonoBoolean manual, MonoBoolean initial, MonoString *name, gint32 *error)
+{
+	if (name != NULL)
+	{
+		g_assertion_message("Named events are not supported by the Unity platform.");
+		return NULL;
+	}
+
+	UnityPalEvent* event = UnityPalEventNew(manual, initial);
+	*error = UnityPalGetLastError();
+	return UnityPalEventHandleNew(event);
+}
+
+gboolean
+ves_icall_System_Threading_Events_SetEvent_internal (gpointer handle)
+{
+	UnityPalErrorCode result = UnityPalEventSet(UnityPalEventHandleGet(handle));
+	return UnityPalSuccess(result);
+}
+
+gboolean
+ves_icall_System_Threading_Events_ResetEvent_internal (gpointer handle)
+{
+	UnityPalErrorCode result = UnityPalEventReset(UnityPalEventHandleGet(handle));
+	return UnityPalSuccess(result);
+}
+
+void
+ves_icall_System_Threading_Events_CloseEvent_internal (gpointer handle)
+{
+	UnityPalHandleDestroy(handle);
+}
+
+gpointer
+ves_icall_System_Threading_Events_OpenEvent_internal (MonoString *name, gint32 rights, gint32 *error)
+{
+	g_assertion_message("Named events are not supported by the Unity platform.");
+	return NULL;
+}
+
+MonoW32HandleNamespace*
+mono_w32event_get_namespace (MonoW32HandleNamedEvent *event)
+{
+	g_assertion_message("Named events are not supported by the Unity platform.");
+	return NULL;
+}
