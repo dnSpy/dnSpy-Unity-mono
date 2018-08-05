a0d66c435cc303e93490ed54bfa455506fcfb17e
de4dot <de4dot@gmail.com>
2018-08-05 11:58:55 +0200

    Add Unity files (unity-2017.1.2-mbe), commit hash 817bc9cdc770971f138ccc217d2b8c35efa2c585

---
+++ unity-2017.1.2-mbe/mono/mini/mini-unity.c
@@ -0,0 +1,29 @@
+#include <config.h>
+#include <mono/mini/mini.h>
+
+#ifdef WIN32
+
+extern LONG CALLBACK seh_vectored_exception_handler(EXCEPTION_POINTERS* ep);
+LONG mono_unity_seh_handler(EXCEPTION_POINTERS* ep)
+{
+#if defined(TARGET_X86) || defined(TARGET_AMD64)
+	return seh_vectored_exception_handler(ep);
+#else
+	g_assert_not_reached();
+#endif
+}
+
+int (*gUnhandledExceptionHandler)(EXCEPTION_POINTERS*) = NULL;
+
+void mono_unity_set_unhandled_exception_handler(void* handler)
+{
+	gUnhandledExceptionHandler = handler;
+}
+
+#endif // WIN32
+
+MONO_API void
+mono_unity_jit_cleanup (MonoDomain *domain)
+{
+	mono_jit_cleanup (domain);
+}
---
+++ unity-2017.1.2-mbe/mono/sgen/sgen-thread-pool.h
@@ -0,0 +1,46 @@
+/*
+ * sgen-thread-pool.h: Threadpool for all concurrent GC work.
+ *
+ * Copyright (C) 2015 Xamarin Inc
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+#ifndef __MONO_SGEN_THREAD_POOL_H__
+#define __MONO_SGEN_THREAD_POOL_H__
+
+typedef struct _SgenThreadPoolJob SgenThreadPoolJob;
+
+typedef void (*SgenThreadPoolJobFunc) (void *thread_data, SgenThreadPoolJob *job);
+
+struct _SgenThreadPoolJob {
+	const char *name;
+	SgenThreadPoolJobFunc func;
+	size_t size;
+	volatile gint32 state;
+};
+
+typedef void (*SgenThreadPoolThreadInitFunc) (void*);
+typedef void (*SgenThreadPoolIdleJobFunc) (void*);
+typedef gboolean (*SgenThreadPoolContinueIdleJobFunc) (void);
+
+void sgen_thread_pool_init (int num_threads, SgenThreadPoolThreadInitFunc init_func, SgenThreadPoolIdleJobFunc idle_func, SgenThreadPoolContinueIdleJobFunc continue_idle_func, void **thread_datas);
+
+void sgen_thread_pool_shutdown (void);
+
+SgenThreadPoolJob* sgen_thread_pool_job_alloc (const char *name, SgenThreadPoolJobFunc func, size_t size);
+/* This only needs to be called on jobs that are not enqueued. */
+void sgen_thread_pool_job_free (SgenThreadPoolJob *job);
+
+void sgen_thread_pool_job_enqueue (SgenThreadPoolJob *job);
+/* This must only be called after the job has been enqueued. */
+void sgen_thread_pool_job_wait (SgenThreadPoolJob *job);
+
+void sgen_thread_pool_idle_signal (void);
+void sgen_thread_pool_idle_wait (void);
+
+void sgen_thread_pool_wait_for_all_jobs (void);
+
+gboolean sgen_thread_pool_is_thread_pool_thread (MonoNativeThreadId thread);
+
+#endif
