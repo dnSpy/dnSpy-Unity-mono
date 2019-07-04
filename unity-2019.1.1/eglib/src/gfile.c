a5c5eef4d5e3e3c2bd4ec498a5249018e6cabbe6
de4dot <de4dot@gmail.com>
2019-07-04 21:46:53 +0200

    Add Unity files (unity-2019.1.1), commit hash 3a543c233aa42c6a5c786118747d73140745427b

---
+++ unity-2019.1.1/eglib/src/gfile.c
@@ -0,0 +1,94 @@
+/*
+ * File utility functions.
+ *
+ * Author:
+ *   Gonzalo Paniagua Javier (gonzalo@novell.com)
+ *
+ * (C) 2006 Novell, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <config.h>
+#include <glib.h>
+#include <stdio.h>
+#include <errno.h>
+
+GFileError
+g_file_error_from_errno (gint err_no)
+{
+	switch (err_no) {
+	case EEXIST:
+		return G_FILE_ERROR_EXIST;
+	case EISDIR:
+		return G_FILE_ERROR_ISDIR;
+	case EACCES:
+		return G_FILE_ERROR_ACCES;
+	case ENAMETOOLONG:
+		return G_FILE_ERROR_NAMETOOLONG;
+	case ENOENT:
+		return G_FILE_ERROR_NOENT;
+	case ENOTDIR:
+		return G_FILE_ERROR_NOTDIR;
+	case ENXIO:
+		return G_FILE_ERROR_NXIO;
+	case ENODEV:
+		return G_FILE_ERROR_NODEV;
+	case EROFS:
+		return G_FILE_ERROR_ROFS;
+#ifdef ETXTBSY
+	case ETXTBSY:
+		return G_FILE_ERROR_TXTBSY;
+#endif
+	case EFAULT:
+		return G_FILE_ERROR_FAULT;
+#ifdef ELOOP
+	case ELOOP:
+		return G_FILE_ERROR_LOOP;
+#endif
+	case ENOSPC:
+		return G_FILE_ERROR_NOSPC;
+	case ENOMEM:
+		return G_FILE_ERROR_NOMEM;
+	case EMFILE:
+		return G_FILE_ERROR_MFILE;
+	case ENFILE:
+		return G_FILE_ERROR_NFILE;
+	case EBADF:
+		return G_FILE_ERROR_BADF;
+	case EINVAL:
+		return G_FILE_ERROR_INVAL;
+	case EPIPE:
+		return G_FILE_ERROR_PIPE;
+	case EAGAIN:
+		return G_FILE_ERROR_AGAIN;
+	case EINTR:
+		return G_FILE_ERROR_INTR;
+	case EIO:
+		return G_FILE_ERROR_IO;
+	case EPERM:
+		return G_FILE_ERROR_PERM;
+	case ENOSYS:
+		return G_FILE_ERROR_NOSYS;
+	default:
+		return G_FILE_ERROR_FAILED;
+	}
+}
+
+
