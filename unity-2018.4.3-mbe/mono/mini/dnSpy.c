e4645915f3b738955208b951ea15f54cdfcc5483
de4dot <de4dot@gmail.com>
2019-07-04 21:56:54 +0200

    Patch Unity files (unity-2018.4.3-mbe)

---
+++ unity-2018.4.3-mbe/mono/mini/dnSpy.c
@@ -0,0 +1,9 @@
+﻿#include <mono/metadata/profiler.h>
+#include <mono/metadata/mono-debug.h>
+#include "debugger-agent.h"
+#define DNUNITYRT 1
+typedef void *MonoLegacyProfiler;
+typedef void (*MonoLegacyProfileFunc) (MonoLegacyProfiler *prof);
+MONO_API void mono_profiler_install (MonoLegacyProfiler *prof, MonoLegacyProfileFunc callback);
+#define DEFINED_LEGACY_PROFILER
+#include "../dnSpyFiles/dnSpy.c"
